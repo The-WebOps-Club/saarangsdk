6ebf83163abb259509be980fce08ccd0a7daa46d
cooliscool <sasebot@gmail.com>
2015-07-02 01:05:37 +0530

    Comments Activity

--- saarangsdk/src/main/java/org/saarang/saarangsdk/Views/NonSwipeableViewPager.java
+++ saarangsdk/src/main/java/org/saarang/saarangsdk/Views/NonSwipeableViewPager.java
@@ -20,7 +20,7 @@ public class NonSwipeableViewPager extends ViewPager {
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent event) {
-        // Never allow swiping to switch between pages
+        // Never allow swiping to switch between  pages
         return false;
     }
 
