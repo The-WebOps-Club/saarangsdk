ea242b0377ebd3a1f3b2dbb6ecc16bfdcaf4cd95
cooliscool <sasebot@gmail.com>
2015-07-03 00:51:55 +0530

    ulta Walls and People. TimeHelper in sdk

---
+++ saarangsdk/src/main/java/org/saarang/saarangsdk/Helpers/TimeHelper.java
@@ -0,0 +1,133 @@
+package org.saarang.saarangsdk.Helpers;
+
+import android.content.Context;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+
+public class TimeHelper {
+
+    private static String LOG_TAG = "TimeHelper";
+
+    public TimeHelper(){
+    }
+
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+    //function for getting Relative time from provided time formatted string directly.
+
+    public static String getRelative(String timeString){
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        long time=1999999999;
+        try {
+            Date date = (Date) formatter.parse(timeString);
+            time = date.getTime();
+        }
+        catch (Exception e){
+
+        }
+
+        if (time < 1000000000000L) {
+            // if timestamp given in seconds, convert to millis
+            time *= 1000;
+        }
+
+        long now = System.currentTimeMillis();
+
+        now-=5.5*60*60*1000; //correction for time zone . Decrease now by 5 : 30 hours.
+
+        if (time > now || time <= 0) {
+            return null;
+        }
+
+        final long diff = now - time;
+        if (diff < MINUTE_MILLIS) {
+            return "Just now";
+        } else if (diff < 2 * MINUTE_MILLIS) {
+            return "A minute ago";
+        } else if (diff < 50 * MINUTE_MILLIS) {
+            return diff / MINUTE_MILLIS + " minutes ago";
+        } else if (diff < 90 * MINUTE_MILLIS) {
+            return "An hour ago";
+        } else if (diff < 24 * HOUR_MILLIS) {
+            return diff / HOUR_MILLIS + " hours ago";
+        } else if (diff < 48 * HOUR_MILLIS) {
+            return "Yesterday";
+        } else {
+            return diff / DAY_MILLIS + " days ago";
+        }
+
+    }
+
+    // function for getting timestamp from the provided time.
+    public static long getTimeStamp(String time){
+        DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        try {
+            Date date = (Date) formatter.parse(time);
+            return date.getTime();
+        }
+        catch (Exception e){
+
+        }
+        return 1999999999;
+
+    }
+
+    //function for getting Relative time from timestamp. (With respect to current time.)
+
+    public static String getTimeAgo(long time) {
+
+
+        if (time < 1000000000000L) {
+            // if timestamp given in seconds, convert to millis
+            time *= 1000;
+        }
+
+        long now = System.currentTimeMillis();
+        if (time > now || time <= 0) {
+            return null;
+        }
+
+        final long diff = now - time;
+        if (diff < MINUTE_MILLIS) {
+            return "Just now";
+        } else if (diff < 2 * MINUTE_MILLIS) {
+            return "A minute ago";
+        } else if (diff < 50 * MINUTE_MILLIS) {
+            return diff / MINUTE_MILLIS + " minutes ago";
+        } else if (diff < 90 * MINUTE_MILLIS) {
+            return "An hour ago";
+        } else if (diff < 24 * HOUR_MILLIS) {
+            return diff / HOUR_MILLIS + " hours ago";
+        } else if (diff < 48 * HOUR_MILLIS) {
+            return "Yesterday";
+        } else {
+            return diff / DAY_MILLIS + " days ago";
+        }
+    }
+
+
+
+    //function for getting Date and Time from timestamp
+    public static  String getDateCurrentTimeZone(long timestamp) {
+        try{
+            Calendar calendar = Calendar.getInstance();
+            TimeZone tz = TimeZone.getDefault();
+            calendar.setTimeInMillis(timestamp * 1000);
+            calendar.add(Calendar.MILLISECOND, tz.getOffset(calendar.getTimeInMillis()));
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+            Date currenTimeZone = (Date) calendar.getTime();
+            return sdf.format(currenTimeZone);
+        }catch (Exception e) {
+        }
+        return "";
+    }
+
+}
